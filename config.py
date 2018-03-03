35c6681ff440714f217b2f1e12831eb4f5f82c92
zer0ttl <sudheer.shirsath+zer0ttl@gmail.com>
2018-03-03 17:45:34 +0530

    database connections initialized and models created

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'y0u-wi11-n3v3r-gu3$$'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
