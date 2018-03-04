350f91c23e6757dbebb60e18e5fcf4b6896f0c5a
zer0ttl <sudheer.shirsath+zer0ttl@gmail.com>
2018-03-04 20:11:45 +0530

    made changes to models.py

--- app/models.py
+++ app/models.py
@@ -1,21 +1,33 @@
 from datetime import datetime
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    email = db.Column(db.String(128), index=True, unique=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    password_hash = db.Column(db.String(256))
+    created_at = db.Column(db.DateTime(), server_default=db.func.now())
+    modified_at = db.Column(db.DateTime(),
+                            server_default=db.func.now(),
+                            server_onupdate=db.func.now())
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-class Post(db.Model):
+
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    isbn = db.Column(db.String(64), unique=True)
+    title = db.Column(db.String(256))
+    author = db.Column(db.String(256))
+    year_published = db.Column(db.String(16))
+    created_at = db.Column(db.DateTime(), server_default=db.func.now())
+    modified_at = db.Column(db.DateTime(),
+                            server_default=db.func.now(),
+                            server_onupdate=db.func.now())
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return '<Book {}>'.format(self.title)
