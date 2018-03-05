83dd0d93a83b84a49c68f1a6fd25d4c8178dd509
zer0ttl <sudheer.shirsath+zer0ttl@gmail.com>
2018-03-05 18:47:33 +0530

    initialized the app.db, added script.sh to run flask application, made changes to models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from app import db
 
 
@@ -9,6 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(128), index=True, unique=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(256))
+    reviews = db.relationship("Review", backref="user", lazy=True)
     created_at = db.Column(db.DateTime(), server_default=db.func.now())
     modified_at = db.Column(db.DateTime(),
                             server_default=db.func.now(),
@@ -22,12 +22,37 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     isbn = db.Column(db.String(64), unique=True)
     title = db.Column(db.String(256))
-    author = db.Column(db.String(256))
-    year_published = db.Column(db.String(16))
     created_at = db.Column(db.DateTime(), server_default=db.func.now())
+    # author = db.relationship("Author", backref="book", lazy=False)
+    year_published = db.Column(db.Integer,
+                               db.ForeignKey("year.id"),
+                               nullable=False)
     modified_at = db.Column(db.DateTime(),
                             server_default=db.func.now(),
                             server_onupdate=db.func.now())
 
     def __repr__(self):
         return '<Book {}>'.format(self.title)
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    books = db.relationship("Book", backref="author", lazy=True)
+
+    def __repr__(self):
+        return '<Author> {}>'.format(self.name)
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.Text)
+    book_id = db.Column
+    user_id = db.Column
+    created_at = db.Column(db.DateTime(), server_default=db.func.now())
+    modified_at = db.Column(db.DateTime(),
+                            server_default=db.func.now(),
+                            server_onupdate=db.func.now())
+
+class Year(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer, unique=True)
---
+++ script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export FLASK_APP="application.py"
+export FLASK_DEBUG=1
